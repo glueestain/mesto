cb8c9e884498fa2f989fd07179e9e8df7d22204d
glueestain <yakimlyuf@yandex.ru>
2023-01-16 20:32:00 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,16 @@ const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__description");
 const buttonEdit = document.querySelector(".profile__edit-button");
 
-const popupEdit = document.querySelector(".popup_edit");
+const popupEdit = document.querySelector(".popup");
 const popupEditButtonClose = popupEdit.querySelector(".popup__btn-close");
-const popupInputName = popupEdit.querySelector(".popup__input_name");
-const popupInputDescription = popupEdit.querySelector(".popup__input_description");
+const popupInputName = popupEdit.querySelector(".popup__input_item_name");
+const popupInputDescription = popupEdit.querySelector(".popup__input_item_description");
 const popupEditForm = popupEdit.querySelector(".popup__form");
-
+const popupOverlay = popupEdit.querySelector('.popup__overlay')
 
 const openPopup = function(item){
     item.classList.add("popup_opened");
-    document.addEventListener('keyup', handleKeyUp);
+    openEditPopup();
 }
 const openEditPopup = function(){
     popupInputName.value = profileName.textContent;
@@ -21,7 +21,6 @@ const openEditPopup = function(){
 
 const closePopup = function (item) {
     item.classList.remove("popup_opened");
-    document.removeEventListener('keyup', handleKeyUp)
 }
 
 function editPopupSubmit(evt) {
@@ -32,12 +31,15 @@ function editPopupSubmit(evt) {
 }
 
 buttonEdit.addEventListener("click", function() {
-    openEditPopup();
     openPopup(popupEdit);
+    popupOverlay.style.display = 'block';
 });
 
 popupEditButtonClose.addEventListener("click", function() {
     closePopup(popupEdit);
+    popupOverlay.style.display = 'none';
 });
 
 popupEditForm.addEventListener("submit", editPopupSubmit);
+
+
