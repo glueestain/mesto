88bbce7ce7cdb50a6e07be76e786e03eee8183c6
glueestain <yakimlyuf@yandex.ru>
2023-01-17 21:30:26 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,6 @@ const popupEditButtonClose = popupEdit.querySelector(".popup__btn-close");
 const popupInputName = popupEdit.querySelector(".popup__input_item_name");
 const popupInputDescription = popupEdit.querySelector(".popup__input_item_description");
 const popupEditForm = popupEdit.querySelector(".popup__form");
-const popupOverlay = popupEdit.querySelector('.popup__overlay')
 
 const openPopup = function(item){
     item.classList.add("popup_opened");
@@ -32,12 +31,10 @@ function editPopupSubmit(evt) {
 
 buttonEdit.addEventListener("click", function() {
     openPopup(popupEdit);
-    popupOverlay.style.display = 'block';
 });
 
 popupEditButtonClose.addEventListener("click", function() {
     closePopup(popupEdit);
-    popupOverlay.style.display = 'none';
 });
 
 popupEditForm.addEventListener("submit", editPopupSubmit);
